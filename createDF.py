6273927b2d9faccb99c6baf6629edc63ece70ff6
jameson-c <jamesonarmstrongcarter@gmail.com>
2021-10-04 11:39:15 -0400

    Creates MainFrame with tag fields for keyword categories which yielded each result.

--- createDF.py
+++ createDF.py
@@ -15,7 +15,6 @@ Output: pandas dataframe MainFrame, written to .csv
 import getGoogleMaps as getGM
 import pandas as pd
 import time
-import sqlite3
 
 with open('Keywords.csv', 'r') as f:
     keywords = []
@@ -29,7 +28,7 @@ MainFrame = pd.DataFrame()
 catIndex = 1
 for keyword in keywords[1:]:
     
-    results = getGM.getMapData('AIzaSyDcMOcdHKsWfOzxGB9wfQfgSjFOeTm1onY',
+    results = getGM.getMapData('AIzaSyCjLXAhl8eQajfsKWIscyNk4Te1wnOdCNM',
                                '40.440600,-79.995900', keyword, '50000')
     data = results[0]
     data['category'] = categories[catIndex]
@@ -40,7 +39,7 @@ for keyword in keywords[1:]:
     # initial API call. So we check to see whether there is a nextToken for each
     if nextToken is not None:
         time.sleep(2) # Need to introduce this so that API call ready for token
-        results2 = getGM.getMapData('AIzaSyDcMOcdHKsWfOzxGB9wfQfgSjFOeTm1onY',
+        results2 = getGM.getMapData('AIzaSyCjLXAhl8eQajfsKWIscyNk4Te1wnOdCNM',
                                '40.440600,-79.995900', keyword, '50000', 
                                nextToken)
         data2 = results2[0]
@@ -49,46 +48,40 @@ for keyword in keywords[1:]:
         
         if nextToken is not None:
             time.sleep(2)
-            results3 = getGM.getMapData('AIzaSyDcMOcdHKsWfOzxGB9wfQfgSjFOeTm1onY',
+            results3 = getGM.getMapData('AIzaSyCjLXAhl8eQajfsKWIscyNk4Te1wnOdCNM',
                                '40.440600,-79.995900', keyword, '50000', 
                                nextToken)
             data3 = results3[0]
             data3['category'] = categories[catIndex]
             nextToken = results3[1]
-    
+       
     try:
-        MainFrame = MainFrame.append([data, data2, data3], ignore_index=True)
+        MainFrame = MainFrame.append([data, data2, data3], ignore_index = True)
     except:
         try:
-            MainFrame = MainFrame.append([data, data2], ignore_index=True)
+            MainFrame = MainFrame.append([data, data2], ignore_index = True)
         except:
-            MainFrame = MainFrame.append([data], ignore_index=True)
+            MainFrame = MainFrame.append([data], ignore_index = True)
     
     # Tick up the index for 'categories' to get the category for the next keyword.        
     catIndex += 1
             
-
+   
 # Retain Pittsburgh addresses
 MainFrame = MainFrame[MainFrame['vicinity'].str.contains('Pittsburgh')]
-
-# Check for duplicates with different categories.
-rowCount = 0
-for row in MainFrame.duplicated(subset=['place_id']):
-    if row == True:
-        ID = MainFrame.iloc[rowCount]['place_id']
-        cat2 = MainFrame.iloc[rowCount]['category']
-        MainFrame.loc[MainFrame['place_id'] == ID,'Category_2'] = cat2
-    rowCount += 1
-
-# Drop results with the same category twice.
-MainFrame = MainFrame.drop(MainFrame.loc[MainFrame['category']==MainFrame['Category_2']].index)
-
-# Drop duplicated results
-MainFrame = MainFrame.drop_duplicates()
-
 # Drop results with a price level listed (Gets rid of most of the restaurants)
 MainFrame = MainFrame.drop(MainFrame.loc[MainFrame['price_level']>=1].index)
+# Drop price level, if it exists
+MainFrame = MainFrame.loc[:,MainFrame.columns.isin(['latitude','longitude',
+            'vicinity','name','place_id','category',])]
+# Add a dummy column for use in pivot table
+MainFrame['Value'] = int(1)
+# Reshape the mainframe
+MainFrame = MainFrame.pivot_table(
+        index=['name', 'place_id', 'latitude', 'longitude', 'vicinity'], 
+         columns='category', 
+         values='Value').reset_index()
+MainFrame.index.name = MainFrame.columns.name = None
 
-    
-print(MainFrame)
+# Write to CSV
 MainFrame.to_csv('MainFrame.csv')
