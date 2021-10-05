8413f14d238c97b34f90df7d5192962b23929a6e
smaillacheruvu <91279804+smaillacheruvu@users.noreply.github.com>
2021-10-05 11:41:42 -0400

    Update Webscrapingtable_Final.py

--- Webscrapingtable_Final.py
+++ Webscrapingtable_Final.py
@@ -76,6 +76,8 @@ def getAddressCoords(input_address, api_key):
 
 
 # get coords using API key and function defined above 
+# ENTER YOUR API KEY for google maps geocoding as the 
+# SECOND INPUT to the getAddressCoords function below 
 coords = []
 for each in foodsites["Vicinity"]: 
     coord = getAddressCoords(each, "Enter your API key here")  
